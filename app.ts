5cefb3fb32ddb4fd01a31289717226af0eee800c
jrs <nil>
2025-09-16 13:59:11 -0400

    cleanup

--- app.ts
+++ app.ts
@@ -1,22 +1,22 @@
 import { app, BrowserWindow } from 'electron';
 
 const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600
-  });
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-  win.loadFile('index.html');
+    win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
-  createWindow()
+    createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
 })
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit()
+    if (process.platform !== 'darwin') app.quit()
 })
