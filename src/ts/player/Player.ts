9a55e685f12758e0a3bd0e6a5795ab61a22acd27
jrs <nil>
2025-09-18 18:36:22 -0400

    The rotation is a little borked, but it works

--- src/ts/player/Player.ts
+++ src/ts/player/Player.ts
@@ -16,8 +16,6 @@ export default class Player extends PhysicsObject implements ITickedObject {
         const geom = new TeapotGeometry(0.2, 10);
         const material = new THREE.MeshMatcapMaterial();
         this.mesh = new THREE.Mesh(geom, material);
-
-        this.mesh.rotation.x = -Math.PI / 2;
     }
 
     public obj(): THREE.Mesh {
@@ -25,39 +23,31 @@ export default class Player extends PhysicsObject implements ITickedObject {
     }
 
     public tick() {
-        super.tick()
-
-        let direction = this.mesh.getWorldDirection(new THREE.Vector3());
-
-        this.applyImpulse(direction.divideScalar(100));
-
-        if (this.world.engine.input.getKey(Keys.LEFT_ARROW)) {
-            this.rotVel.z += 0.025;
-        } else if (this.world.engine.input.getKey(Keys.RIGHT_ARROW)) {
-            this.rotVel.z -= 0.025;
-        }
+        super.tick();
 
         if (this.world.engine.input.getKey(Keys.W)) {
-            this.rotVel.x -= Math.cos(this.mesh.rotation.z) * 0.025;
-            this.rotVel.y -= Math.sin(this.mesh.rotation.z) * 0.025;
+            this.rotVel.x -= 0.01;
         } else if (this.world.engine.input.getKey(Keys.S)) {
-            this.rotVel.x += Math.cos(this.mesh.rotation.z) * 0.025;
-            this.rotVel.y += Math.sin(this.mesh.rotation.z) * 0.025;
+            this.rotVel.x += 0.01;
+        }
+
+        if (this.world.engine.input.getKey(Keys.LEFT_ARROW)) {
+            this.rotVel.y += 0.01;
+        } else if (this.world.engine.input.getKey(Keys.RIGHT_ARROW)) {
+            this.rotVel.y -= 0.01;
         }
 
         this.rotVel.x *= 0.9;
         this.rotVel.y *= 0.9;
-        this.rotVel.z *= 0.9;
 
-        this.mesh.rotation.x += ((-Math.PI / 2) - this.mesh.rotation.x) * 0.4;
-        this.mesh.rotation.y += (0-this.mesh.rotation.y) * 0.4;
+        this.mesh.rotation.x *= 0.9;
 
-        this.world.camera.position.set(
-            this.mesh.position.x+Math.sin(this.mesh.rotation.z),
-            this.mesh.position.y+0.5,
-            this.mesh.position.z+Math.cos(this.mesh.rotation.z)
-        )
+        const vVel = Math.cos(this.mesh.rotation.x) * 0.01;
+        const hVel = Math.sin(this.mesh.rotation.x) * 0.01;
 
-        this.world.camera.lookAt(this.mesh.position);
+        this.vel.x += Math.sin(this.mesh.rotation.y) * hVel;
+        this.vel.z += Math.cos(this.mesh.rotation.y) * hVel;
+
+        this.vel.y += vVel;
     }
 }
