715dd5f8f620a01e22408f5feb5ffb3703b44556
jrs <nil>
2025-09-18 19:12:19 -0400

    Proper blade

--- src/ts/player/Player.ts
+++ src/ts/player/Player.ts
@@ -8,14 +8,20 @@ import PhysicsObject from '../engine/NewtonObject';
 export default class Player extends PhysicsObject implements ITickedObject {
     public readonly world: IWorld;
     private readonly mesh: THREE.Mesh;
+    private readonly rotor: THREE.Mesh;
 
     constructor(world: IWorld) {
         super();
         this.world = world;
 
         const geom = new TeapotGeometry(0.2, 10);
-        const material = new THREE.MeshMatcapMaterial();
+        const material = new THREE.MeshNormalMaterial();
         this.mesh = new THREE.Mesh(geom, material);
+
+        const rotorGeom = new THREE.BoxGeometry(1, 0.01, 0.05);
+        this.rotor = new THREE.Mesh(rotorGeom, material);
+        this.rotor.position.y = 0.2
+        this.mesh.add(this.rotor);
     }
 
     public obj(): THREE.Mesh {
@@ -25,10 +31,12 @@ export default class Player extends PhysicsObject implements ITickedObject {
     public tick() {
         super.tick();
 
+        this.rotor.rotation.y += 1;
+
         if (this.world.engine.input.getKey(Keys.W)) {
-            this.rotVel.x -= 0.01;
+            this.rotVel.z += 0.01;
         } else if (this.world.engine.input.getKey(Keys.S)) {
-            this.rotVel.x += 0.01;
+            this.rotVel.z -= 0.01;
         }
 
         if (this.world.engine.input.getKey(Keys.LEFT_ARROW)) {
@@ -39,15 +47,19 @@ export default class Player extends PhysicsObject implements ITickedObject {
 
         this.rotVel.x *= 0.9;
         this.rotVel.y *= 0.9;
+        this.rotVel.z *= 0.9;
 
         this.mesh.rotation.x *= 0.9;
+        this.mesh.rotation.z *= 0.9;
 
-        const vVel = Math.cos(this.mesh.rotation.x) * 0.01;
-        const hVel = Math.sin(this.mesh.rotation.x) * 0.01;
+        const vVel = Math.cos(this.mesh.rotation.z) * 0.01;
+        const hVel = Math.sin(this.mesh.rotation.z) * 0.01;
 
-        this.vel.x += Math.sin(this.mesh.rotation.y) * hVel;
-        this.vel.z += Math.cos(this.mesh.rotation.y) * hVel;
+        this.vel.x -= Math.cos(this.mesh.rotation.y) * hVel;
+        this.vel.z += Math.sin(this.mesh.rotation.y) * hVel;
 
         this.vel.y += vVel;
+
+        this.vel.multiplyScalar(0.95);
     }
 }
