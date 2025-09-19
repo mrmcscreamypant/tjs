136801cca545eb3d39c570aab59e6dcdf205451a
jrs <nil>
2025-09-19 13:38:56 -0400

    Fine for now

--- src/ts/particles/steam.ts
+++ src/ts/particles/steam.ts
@@ -8,7 +8,7 @@ export default new QUARKS.ParticleSystem({
     duration: 1,
     looping: true,
     shape: new QUARKS.PointEmitter(),
-    emissionOverTime: new QUARKS.ConstantValue(25),
+    emissionOverTime: new QUARKS.ConstantValue(0.9),
 
     worldSpace: true,
     renderMode: QUARKS.RenderMode.BillBoard,
@@ -22,11 +22,17 @@ export default new QUARKS.ParticleSystem({
     }),
 
     behaviors: [
-        new QUARKS.Noise(
-            new QUARKS.ConstantValue(0.1),
-            new QUARKS.ConstantValue(0.5),
-            new QUARKS.ConstantValue(0.3),
+        new QUARKS.ForceOverLife(
             new QUARKS.ConstantValue(0),
-        )
+            new QUARKS.PiecewiseBezier([[new QUARKS.Bezier(0.5, 1, -1, 0), 0]]),
+            new QUARKS.ConstantValue(0),
+        ),
+        new QUARKS.SizeOverLife(
+            new QUARKS.Vector3Function(
+                new QUARKS.PiecewiseBezier([[new QUARKS.Bezier(1, 1.5, 2, 3), 0]]), // X axis
+                new QUARKS.PiecewiseBezier([[new QUARKS.Bezier(1, 1.5, 2, 3), 0]]), // Y axis
+                new QUARKS.ConstantValue(1), // Z axis
+            ),
+        ),
     ]
 });
