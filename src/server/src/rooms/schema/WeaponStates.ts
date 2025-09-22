39c3f9514a3d42bdcb8bf3275bc281c21834972d
jrs <nil>
2025-09-22 08:55:58 -0400

    projectile rotation

--- src/server/src/rooms/schema/WeaponStates.ts
+++ src/server/src/rooms/schema/WeaponStates.ts
@@ -1,15 +1,28 @@
-import { WeaponState } from "./BattleState";
+import * as THREE from 'three';
+import { WeaponState, Vector3State } from "./BattleState";
 import { type } from '@colyseus/schema';
 
 export class DebugWeaponState extends WeaponState {
     @type("number") age: number = 0;
+    @type(Vector3State) rotation: Vector3State;
 
     init() {
+        this.rotation = this.owner.rotation.clone();
         this.position = this.owner.position.clone();
     }
 
     public tick(): boolean {
-        this.position.z += 0.1;
+        const quat = new THREE.Quaternion().setFromEuler(
+            new THREE.Euler().setFromVector3(
+                new THREE.Vector3().copy(this.rotation)
+            )
+        );
+        const vec = new THREE.Vector3(-1, 0, 0).applyQuaternion(quat);
+
+        this.position.x += vec.x
+        this.position.y += vec.y
+        this.position.z += vec.z
+
         this.age += 1;
         if (this.age > 10) {
             return false;
